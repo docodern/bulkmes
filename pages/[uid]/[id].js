711915a178ff21501b35489e0ec88cdf5405b201
Dmitrijs Osipovs <dmitrijsosipov@gmail.com>
2022-10-17 23:02:10 +0300

    stripe testing

--- pages/[uid]/[id].js
+++ pages/[uid]/[id].js
@@ -1,14 +1,32 @@
 import Head from "next/head";
 import { SliceZone } from "@prismicio/react";
 import * as prismicH from "@prismicio/helpers";
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import useSWR from "swr";
 
 import { createClient } from "../../prismicio";
 import { components } from "../../slices";
 import { Layout } from "../../components/Layout";
+import { fetcher } from "../../utils/helpers"
 
 
 const IDpage = ({ page, navigation, settings }) => {
 
+  const {
+    query: { session_id },
+  } = useRouter();
+
+  const { data, error } = useSWR(
+    () => `/api/checkout_sessions/${session_id}`,
+    fetcher
+  );
+
+  useEffect(() => {
+    if (data) {
+      console.log("DATA: " + data)
+    }
+  }, [data]);
   
   return (
     <Layout
@@ -22,6 +40,25 @@ const IDpage = ({ page, navigation, settings }) => {
           {prismicH.asText(settings.data.siteTitle)}
         </title>
       </Head>
+      <div className="container xl:max-w-screen-xl mx-auto py-12 px-6 text-center">
+      {error ? (
+        <div className="p-2 rounded-md bg-rose-100 text-rose-500 max-w-md mx-auto">
+          <p className="text-lg">Sorry, something went wrong!</p>
+        </div>
+      ) : !data ? (
+        <div className="p-2 rounded-md bg-gray-100 text-gray-500 max-w-md mx-auto">
+          <p className="text-lg animate-pulse">Loading...</p>
+        </div>
+      ) : (
+        <div className="py-4 px-8 rounded-md bg-gray-100 max-w-lg mx-auto">
+          <h2 className="text-4xl font-semibold flex flex-col items-center space-x-1">
+            
+            <span>Thanks for your order!</span>
+          </h2>
+          <p className="text-lg mt-3">Check your inbox for the receipt.</p>
+        </div>
+      )}
+    </div>
       <SliceZone slices={page.data.slices} components={components} />
     </Layout>
   );
