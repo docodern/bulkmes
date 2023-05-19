567f10058f51f3ee0c303a9b4646650fee862024
Dmitrijs Osipovs <dmitrijsosipov@gmail.com>
2023-05-19 22:56:22 +0300

    PopUp added to Contact form

--- .slicemachine/assets/slices/Contacts/index.stories.js
+++ .slicemachine/assets/slices/Contacts/index.stories.js
@@ -5,5 +5,5 @@ export default {
 }
 
 
-export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{}],"primary":{"title":[{"type":"heading2","text":"Student","spans":[]}],"description":[{"type":"paragraph","text":"Non est ullamco velit voluptate dolore esse sunt enim cillum eu. Enim occaecat adipisicing exercitation eu quis voluptate eu deserunt minim dolor. Occaecat ipsum eiusmod consectetur.","spans":[]}],"title2":[{"type":"heading2","text":"Birds","spans":[]}],"adress":"gold","email":"hurried","telephone":"top","nameField":"height","emailField":"eight","textField":"factor","buttonText":"activity"},"slice_type":"contacts","id":"_Default"}} />
+export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{}],"primary":{"title":[{"type":"heading2","text":"Education","spans":[]}],"description":[{"type":"paragraph","text":"Ipsum deserunt labore adipisicing labore.","spans":[]}],"title2":[{"type":"heading2","text":"Spell","spans":[]}],"adress":"drawn","email":"brick","telephone":"develop","nameField":"glass","emailField":"cloud","textField":"ten","buttonText":"rain","popup_error_title":"worse","popup_error_text":"as","popup_title":"goose","popup_text":"equal","popup_button":"drink"},"id":"_Default","slice_type":"contacts"}} />
 _Default.storyName = ''
